2301a93b6a9ec65d264ff483de33502fbc67f70a
ThatGuy <104093092+ThatGuyOliver@users.noreply.github.com>
2026-08-19 22:05:58 +0200

    Add Sensor

--- components/diehl/diehl.h
+++ components/diehl/diehl.h
@@ -16,6 +16,17 @@ class DiehlComponent : public PollingComponent, public uart::UARTDevice {
   time::RealTimeClock *get_time() const { return time_; }
   void set_power_sensor(sensor::Sensor *sensor) { this->power_sensor_ = sensor; }
   void set_day_energy_sensor(sensor::Sensor *sensor) { this->day_energy_sensor_ = sensor; }
+  void set_status_sensor(sensor::Sensor *sensor) { this->status_sensor_ = sensor; }
+  void set_event_sensor(sensor::Sensor *sensor) { this->event_sensor_ = sensor; }
+  void set_dc_voltage_sensor(sensor::Sensor *sensor) { this->dc_voltage_sensor_ = sensor; }
+  void set_dc_current_sensor(sensor::Sensor *sensor) { this->dc_current_sensor_ = sensor; }
+  void set_dc_power_sensor(sensor::Sensor *sensor) { this->dc_power_sensor_ = sensor; }
+  void set_ac_voltage_sensor(sensor::Sensor *sensor) { this->ac_voltage_sensor_ = sensor; }
+  void set_ac_current_sensor(sensor::Sensor *sensor) { this->ac_current_sensor_ = sensor; }
+  void set_ac_power_sensor(sensor::Sensor *sensor) { this->ac_power_sensor_ = sensor; }
+  void set_temp_1_sensor(sensor::Sensor *sensor) { this->temp_1_sensor_ = sensor; }
+  void set_temp_2_sensor(sensor::Sensor *sensor) { this->temp_2_sensor_ = sensor; }
+  void set_temp_3_sensor(sensor::Sensor *sensor) { this->temp_3_sensor_ = sensor; }
 
   void dump_config() override;
   float get_setup_priority() const override;
@@ -29,6 +40,17 @@ class DiehlComponent : public PollingComponent, public uart::UARTDevice {
   // Sensors
   sensor::Sensor *power_sensor_{nullptr};
   sensor::Sensor *day_energy_sensor_{nullptr};
+  sensor::Sensor *status_sensor_{nullptr};
+  sensor::Sensor *event_sensor_{nullptr};
+  sensor::Sensor *dc_voltage_sensor_{nullptr};
+  sensor::Sensor *dc_current_sensor_{nullptr};
+  sensor::Sensor *dc_power_sensor_{nullptr};
+  sensor::Sensor *ac_voltage_sensor_{nullptr};
+  sensor::Sensor *ac_current_sensor_{nullptr};
+  sensor::Sensor *ac_power_sensor_{nullptr};
+  sensor::Sensor *temp_1_sensor_{nullptr}; //Unknown
+  sensor::Sensor *temp_2_sensor_{nullptr};
+  sensor::Sensor *temp_3_sensor_{nullptr};
 
   // Methods
   void update_time();
--- components/diehl/sensor.py
+++ components/diehl/sensor.py
@@ -17,6 +17,17 @@ CODEOWNERS = ["@ThatGuyOliver"]
 DEPENDENCIES = ["uart", "time"]
 
 CONF_DAY_ENERGY = 'day_energy'
+CONF_STATUS = "status"
+CONF_EVENT = "event"
+CONF_DC_VOLTAGE = "dc_voltage"
+CONF_DC_CURRENT = "dc_current"
+CONF_DC_POWER = "dc_power"
+CONF_AC_VOLTAGE = "ac_voltage"
+CONF_AC_CURRENT = "ac_current"
+CONF_AC_POWER = "ac_power"
+CONF_TEMP_1 = "temp_1"
+CONF_TEMP_2 = "temp_2"
+CONF_TEMP_3 = "temp_3"
 
 diehl_ns = cg.esphome_ns.namespace("diehl")
 DiehlComponent = diehl_ns.class_(
@@ -41,6 +52,70 @@ CONFIG_SCHEMA = (
                 device_class=DEVICE_CLASS_POWER,
                 state_class=STATE_CLASS_MEASUREMENT,
                 unit_of_measurement=UNIT_WATT,
+            ),
+            cv.Optional(CONF_STATUS): sensor.sensor_schema(
+                 accuracy_decimals=0,
+                    device_class=DEVICE_CLASS_NONE,  # of een passende class
+                 state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_EVENT): sensor.sensor_schema(
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_NONE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_DC_VOLTAGE): sensor.sensor_schema(
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_VOLTAGE,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_VOLT,
+            ),
+            cv.Optional(CONF_DC_CURRENT): sensor.sensor_schema(
+                accuracy_decimals=2,
+                device_class=DEVICE_CLASS_CURRENT,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_AMPERE,
+            ),
+            cv.Optional(CONF_DC_POWER): sensor.sensor_schema(
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_POWER,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_WATT,
+            ),
+            cv.Optional(CONF_AC_VOLTAGE): sensor.sensor_schema(
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_VOLTAGE,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_VOLT,
+            ),
+            cv.Optional(CONF_AC_CURRENT): sensor.sensor_schema(
+                accuracy_decimals=2,
+                device_class=DEVICE_CLASS_CURRENT,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_AMPERE,
+            ),
+            cv.Optional(CONF_AC_POWER): sensor.sensor_schema(
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_POWER,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_WATT,
+            ),
+            cv.Optional(CONF_TEMP_1): sensor.sensor_schema(
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_CELSIUS,
+            ),
+            cv.Optional(CONF_TEMP_2): sensor.sensor_schema(
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_CELSIUS,
+            ),
+            cv.Optional(CONF_TEMP_3): sensor.sensor_schema(
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+                unit_of_measurement=UNIT_CELSIUS,
             )
         }
     )
@@ -61,12 +136,24 @@ async def to_code(config):
     cg.add(var.set_time(time_))
 
     # Standard sensors
-    for key in [
-        CONF_DAY_ENERGY,
-        CONF_POWER
-    ]:
-        if key not in config:
-            continue
-        conf = config[key]
-        sens = await sensor.new_sensor(conf)
-        cg.add(getattr(var, f"set_{key}_sensor")(sens))
+    # Standard sensors
+for key in [
+    CONF_DAY_ENERGY,
+    CONF_POWER,
+    CONF_STATUS,
+    CONF_EVENT,
+    CONF_DC_VOLTAGE,
+    CONF_DC_CURRENT,
+    CONF_DC_POWER,
+    CONF_AC_VOLTAGE,
+    CONF_AC_CURRENT,
+    CONF_AC_POWER,
+    CONF_TEMP_1,
+    CONF_TEMP_2,
+    CONF_TEMP_3,
+]:
+    if key not in config:
+        continue
+    conf = config[key]
+    sens = await sensor.new_sensor(conf)
+    cg.add(getattr(var, f"set_{key}_sensor")(sens))
